dbd3c8bd771aa8bb0f96ecd98c383f6575e2e045
zain <zainahmed8@hotmail.com>
2024-09-12 20:00:58 -0500

    added csv data

--- csvdataingest.py
+++ csvdataingest.py
@@ -1,3 +1,26 @@
 from urllib.request import urlopen
 import json
+import pandas as pd
+import requests
 
+methods = ['car_data', 'drivers', 'intervals', 'laps', 'location', 'pit', 'position', 'race_control', 'sessions', 'stints', 'weather']
+
+base_url = "https://api.openf1.org/v1/{method}?csv=true"
+
+for method in methods:
+    url = base_url.format(method=method)
+    
+    try:
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        output_path = f'{method}.csv'
+        
+        with open(output_path, 'wb') as file:
+            file.write(response.content)
+        
+        print(f"CSV data for '{method}' has been saved to {output_path}")
+    
+    except Exception as e:
+        print(f"Failed to download or save data for '{method}': {e}")
